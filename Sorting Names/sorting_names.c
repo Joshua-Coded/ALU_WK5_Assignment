f7ad4fe551860186c9a7e4b9caa309540923e723
GChukwudi <g.chukwudi@alustudent.com>
2024-10-11 15:39:46 +0200

    doc provided

--- Sorting Names/sorting_names.c
+++ Sorting Names/sorting_names.c
@@ -17,6 +17,7 @@ void swap(char *xp, char *yp);
 
 int main()
 {
+    // array of names to be sorted
     char names[size][MAX_NAME_LENGTH] = {
         "Joshua Alana", "Chinwe Oka", "Chukwudi Oka", "Chinwe Ifexhukwu",
         "Joshua Oka", "Chukwudi Godsfavour", "King David", "Chinwe John",
@@ -25,12 +26,15 @@ int main()
         "Okeke Fiona", "Ishimwe Divine", "Festus Kingsley", "Stephen Adebayo"
     };
 
+    //int size = sizeof(names) / sizeof(names[0]);
     char order[10];
     int ascemding;
 
+    // get the order of sorting from the user
     printf("Enter the order of sorting (asc or desc): ");
     scanf("%s", order);
 
+    // check if the order is valid
     if (strcmp(order, "asc") == 0)
     {
         ascemding = 1;
@@ -45,8 +49,10 @@ int main()
         return 1;
     }
 
+    // sort the names
     bubbleSort(names, size, ascemding);
 
+    // print the sorted names
     printf("Sorted names:\n");
     for (int i = 0; i < size; i++)
     {
@@ -57,8 +63,15 @@ int main()
 
 }
 
+/**
+ * Swap two strings
+ * 
+ * @param xp: first string
+ * @param yp: second string
+ */
 void swap(char *xp, char *yp)
 {
+    // swap the strings
     char temp[MAX_NAME_LENGTH];
     strcpy(temp, xp);
     strcpy(xp, yp);
@@ -74,14 +87,17 @@ void swap(char *xp, char *yp)
  */
 void bubbleSort(char names[][MAX_NAME_LENGTH], int n, int ascemding)
 {
+    // sort the names
     int swapped;
     char temp[MAX_NAME_LENGTH];
 
+    // sort the names
     for (int i = 0; i < n - 1; i++)
     {
         swapped = 0;
         for (int j = 0; j < n - i - 1; j++)
         {
+            // compare the names
             if ((ascemding && strcmp(names[j], names[j + 1]) > 0) || (!ascemding && strcmp(names[j], names[j + 1]) < 0))
             {
                 swap(names[j], names[j + 1]);
